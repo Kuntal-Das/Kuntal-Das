64643c2a6c909cf63b002834a5c67aa4de145a68
Kuntal Das <25ckuntaldas@gmail.com>
2021-05-24 15:39:47 +0530

    interractive header styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,31 +1,81 @@
-import React from "react";
-import {Link} from "react-router-dom"
+import React, { useEffect, useRef, useState } from "react";
+import { NavLink } from "react-router-dom";
 
-const Header = (props) => (
-  <header>
-    <div className="logo">
-      <h2>KD</h2>
-    </div>
-    <button className="nav-toggle" aria-label="toggle navigation">
-      <span className="hamburger"></span>
-    </button>
-    <nav className="nav">
-      <ul className="nav__list">
-        <li className="nav__item">
-          <Link to="/" className="nav__link" >Home</Link>
-        </li>
-        <li className="nav__item">
-          <Link to="/services" className="nav__link" >My Services</Link>
-        </li>
-        <li className="nav__item">
-          <Link to="/about" className="nav__link" >About Me</Link>
-        </li>
-        <li className="nav__item">
-          <Link to="/work" className="nav__link" >My work</Link>
-        </li>
-      </ul>
-    </nav>
-  </header>
-);
+import "../styles/header.scss";
+
+const Header = (props) => {
+  const hamburger = useRef(null);
+  const [isNavOpen, setIsNavOpen] = useState(false);
+
+  const toggleNav = () => setIsNavOpen((prevIsNavOpen) => !prevIsNavOpen);
+
+  const closeNav = () => setIsNavOpen(false);
+
+  useEffect(() => {
+    const ham = hamburger.current;
+    ham.addEventListener("click", toggleNav);
+    return () => ham.removeEventListener("click", toggleNav);
+  }, []);
+
+  return (
+    <header className={`header ${isNavOpen && `nav-open`}`}>
+      <div className="logo">
+        <h2>KD</h2>
+      </div>
+      <button
+        ref={hamburger}
+        className="nav-toggle"
+        aria-label="toggle navigation"
+      >
+        <span className="hamburger"></span>
+      </button>
+      <nav className="nav">
+        <ul className="nav__list">
+          <li className="nav__item">
+            <NavLink
+              to="/"
+              exact={true}
+              activeClassName="current-page"
+              className="nav__link"
+              onClick={closeNav}
+            >
+              Home
+            </NavLink>
+          </li>
+          <li className="nav__item">
+            <NavLink
+              to="/services"
+              activeClassName="current-page"
+              className="nav__link"
+              onClick={closeNav}
+            >
+              My Services
+            </NavLink>
+          </li>
+          <li className="nav__item">
+            <NavLink
+              to="/about"
+              activeClassName="current-page"
+              className="nav__link"
+              onClick={closeNav}
+            >
+              About Me
+            </NavLink>
+          </li>
+          <li className="nav__item">
+            <NavLink
+              to="/work"
+              activeClassName="current-page"
+              className="nav__link"
+              onClick={closeNav}
+            >
+              My work
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
 
 export default Header;
