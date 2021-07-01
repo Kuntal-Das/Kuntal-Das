0874a747f31c9a98d764073ec3192fd16cdfe9a7
Kuntal Das <25ckuntaldas@gmail.com>
2021-07-01 12:04:49 +0530

    added color scheme with ThemeProvider

--- src/App.js
+++ src/App.js
@@ -1,9 +1,51 @@
-import React from "react";
+import React, { Component } from "react";
+import { ThemeProvider } from "styled-components";
+import { HomePage } from "./pages";
+import { light, dark, getRadShadow } from "./colorScheme"
 
-function App() {
-  return (
-    <h1>Hello React-land</h1>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.mql = window.matchMedia('(prefers-color-scheme: dark)')
+    this.state = {
+      theme: { ...light },
+      radShadow: {
+        boxShadowDefault: `0 1rem .5rem -.5rem`,
+        boxShadow: getRadShadow(light.surfaceShadow, light.shadowStrength)
+      }
+    }
+  }
+
+  handleSystemColorSchemeChange = (e) => {
+    if (e.matches && this.state.theme.name === "light") {
+      this.setState({
+        theme: { ...dark },
+        radShadow: getRadShadow(dark.surfaceShadow, dark.shadowStrength)
+      })
+    } else if (!e.matches && this.state.theme.name === "dark") {
+      this.setState({
+        theme: { ...light },
+        radShadow: getRadShadow(light.surfaceShadow, light.shadowStrength)
+      })
+    }
+  }
+
+  componentDidMount() {
+    this.mql.addEventListener('change', this.handleSystemColorSchemeChange);
+  }
+
+  componentWillUnmount = () => {
+    this.mql.removeEventListener('change', this.handleSystemColorSchemeChange);
+  }
+
+  render = () => {
+    console.log(this.state)
+    return (
+      <ThemeProvider theme={{ ...this.state.theme }}>
+        <HomePage />
+      </ThemeProvider>
+    );
+  }
 }
 
 export default App;
