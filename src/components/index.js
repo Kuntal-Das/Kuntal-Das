9b74d6351d26b07c40e42fd6117a07d8b202945f
Kuntal Das <25ckuntaldas@gmail.com>
2021-07-12 21:05:45 +0530

    hero title and illustration updated

--- src/components/index.js
+++ src/components/index.js
@@ -101,11 +101,11 @@ export const TitleH1 = styled.h1`
     line-height: 1.2;
     text-align: center;
     margin: 0.5em 0 1.25em;
+    max-width: 15ch;
 
     @media (min-width:600px){
         margin-top: 0.25em;
         text-align: initial;
-        max-width: 15ch;
     }
 `;
 
@@ -169,14 +169,20 @@ export const ButtonLink = styled(ReactRouterLink).attrs(props => ({
 `
 
 export const HeroIllustration = styled(WorkingInLaptop)`
-    width: 100%;
     /* height: 100%; */
-    color: hsl(235 100% 60%);
     /* color: ${({ theme }) => theme.brand}; */
+    /* color: hsl(235 100% 60%); */
+    width: 100%;
+    color: #6257DB;
     @media (max-width: 599px){
         width: 80%;
         max-width: 400px;
     }
+
+    .outline_black{
+        stroke:${({ theme }) => theme.text2};
+    }
+
 `
 
 export const FlexCol = styled.div.attrs(props => ({
@@ -185,7 +191,8 @@ export const FlexCol = styled.div.attrs(props => ({
     display: flex;
     justify-content: space-evenly;
     align-items: center;
-
+    width: 100%;
+    
     @media (min-width:600px){
         justify-content: initial;
         ${ButtonLink} + ${ButtonLink}{
@@ -198,7 +205,11 @@ export const Group = styled.div`
     display: flex;
     flex-direction: column;
     justify-content: center;
+    align-items: center;    
     padding-bottom: 5vw;
+    @media(min-width:600px){
+        align-items: initial;    
+    }
 
     ${Lead}{
         align-self: center;
